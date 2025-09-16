b8ae42748d700314e2d6788e8c480aeb02f0e755
utky <ilyaletre@gmail.com>
2025-09-16 21:01:48 +0000

    tweak env

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -2,22 +2,23 @@
 # npmでGemini CLIをインストール
 npm install -g '@google/gemini-cli'
 
-# gcloud認証
-gcloud auth login --no-launch-browser
-gcloud auth application-default login --no-launch-browser
-
 # tofu
 LATEST_VERSION=$(curl https://api.github.com/repos/sigstore/cosign/releases/latest | jq -r .tag_name | tr -d "v")
 curl -O -L "https://github.com/sigstore/cosign/releases/latest/download/cosign_${LATEST_VERSION}_amd64.deb"
 sudo dpkg -i cosign_${LATEST_VERSION}_amd64.deb
+rm cosign_${LATEST_VERSION}_amd64.deb
+
 LATEST_VERSION=$(curl --silent https://api.github.com/repos/tofuutils/tenv/releases/latest | jq -r .tag_name)
 curl -O -L "https://github.com/tofuutils/tenv/releases/latest/download/tenv_${LATEST_VERSION}_amd64.deb"
 sudo dpkg -i "tenv_${LATEST_VERSION}_amd64.deb"
-
-rm cosign_${LATEST_VERSION}_amd64.deb
 rm "tenv_${LATEST_VERSION}_amd64.deb"
 
 tenv completion bash > ~/.tenv.completion.bash
 echo "source \$HOME/.tenv.completion.bash" >> ~/.bashrc
 
 tenv tofu install
+
+# gcloud認証
+echo "Please run commands below if you needed."
+echo "gcloud auth login --no-launch-browser"
+echo "gcloud auth application-default login --no-launch-browser"
