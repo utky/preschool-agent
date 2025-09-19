b6fc4f3c516db3220d4f53552667cef0a9836597
utky <ilyaletre@gmail.com>
2025-09-19 21:09:06 +0000

    cloud runのIAMとして許可を入れてみた

--- tf/modules/iap/main.tf
+++ tf/modules/iap/main.tf
@@ -4,11 +4,12 @@ resource "google_project_service" "iap" {
 }
 
 # IAPで保護されたWebアプリへのアクセスを許可するメンバーを設定
-resource "google_iap_web_backend_service_iam_member" "allow_users" {
+resource "google_cloud_run_v2_service_iam_member" "allow_users" {
   for_each = toset(var.iap_allowed_users)
 
-  project             = var.project_id
-  web_backend_service = var.cloud_run_service_name
-  role                = "roles/iap.httpsResourceAccessor"
-  member              = "user:${each.value}"
+  project  = var.project_id
+  location = var.region
+  name     = var.cloud_run_service_name
+  role     = "roles/run.invoker"
+  member   = "user:${each.value}"
 }
--- tf/modules/iap/variables.tf
+++ tf/modules/iap/variables.tf
@@ -1,22 +1,20 @@
 variable "project_id" {
   type        = string
-  description = "Google CloudのプロジェクトID"
-}
-
-variable "cloud_run_service_name" {
-  type        = string
-  description = "The name of the Cloud Run service to protect with IAP."
+  description = "The ID of the Google Cloud project."
 }
 
 variable "region" {
   type        = string
-  description = "The region where the Cloud Run service is deployed."
+  description = "The Google Cloud region."
 }
 
-
+variable "cloud_run_service_name" {
+  type        = string
+  description = "The name of the Cloud Run service."
+}
 
 variable "iap_allowed_users" {
   type        = list(string)
-  description = "IAPでアクセスを許可するユーザーのメールアドレスのリスト"
+  description = "A list of user emails to allow access via IAP."
   default     = []
 }
